e807f4f1dcda156f9e26f4b777e5cdf7c38424d5
KumarSaurabhThakur <160490638+KumarSaurabhThakur@users.noreply.github.com>
2025-04-20 22:08:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let num1=document.querySelector("#num1");
+//JAVASCRIPT PROGRAM FOR THE SIMPLE CALCULATOR
+
+/*let num1=document.querySelector("#num1");
 let num2=document.querySelector("#num2");
 let btnAdd=document.querySelector("#btnAdd");
 let btnSub=document.querySelector("#btnSub");
@@ -6,7 +8,6 @@ let btnMul=document.querySelector("#btnMul");
 let btnDiv=document.querySelector("#btnDiv");
 let result=document.querySelector("#result");
 let resultbox=document.querySelector("#resultbox");
-
 
 btnAdd.addEventListener("click",add);
 btnSub.addEventListener("click",sub);
@@ -43,4 +44,38 @@ function div()
     let b=parseInt(num2.value);
     resultbox.value=a/b;
     result.innerText=a/b;
-}
+}*/
+
+
+
+//JAVASCRIPT PROGRAM FOR THE REAL CALCULATOR
+
+let input = document.getElementById("inputBox");
+let buttons = document.querySelectorAll("button");
+
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach((button) => {
+    button.addEventListener("click", function (event) {
+        event.preventDefault();
+    });
+
+    button.addEventListener("click", (e) => {
+        if (e.target.innerHTML == "=") { 
+            string = eval(string);
+            input.value = string;
+        }
+        else if (e.target.innerHTML == "AC") {
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.innerHTML == "DE"){
+            string = string.substring(0, string.length - 1);
+            input.value = string;
+        }
+        else{
+        string += e.target.innerHTML;
+        input.value = string;
+    }
+    })
+})
